9cd5ab939321404ca816a4e9dafa71cf45529b95
sangwon91 <sangwonyoo91@cknb.co.kr>
2022-05-24 13:56:53 +0900

    Add Layout overlap

--- app/src/main/java/com/example/apptest0523/ScanCamera.java
+++ app/src/main/java/com/example/apptest0523/ScanCamera.java
@@ -2,7 +2,10 @@ package com.example.apptest0523;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,11 @@ public class ScanCamera extends AppCompatActivity {
         setContentView(R.layout.activity_surface_view);
         scanCameraSurfaceView = findViewById(R.id.sv);
         context = this;
+
+        //Layout 합치기
+        LayoutInflater inflater = getLayoutInflater();
+        View scancamera_view = inflater.inflate(R.layout.activity_scancamera,null);
+        addContentView(scancamera_view,new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
 
     }//onCreate
 
