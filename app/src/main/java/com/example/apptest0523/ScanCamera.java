00dd28594ff8af408a755b49a89676d4f410dff9
sangwon91 <sangwonyoo91@cknb.co.kr>
2022-05-30 17:11:40 +0900

    Test Image Size
    화면의 이미지 크기를 측정하는 연습을 해보았음.

--- app/src/main/java/com/example/apptest0523/ScanCamera.java
+++ app/src/main/java/com/example/apptest0523/ScanCamera.java
@@ -2,12 +2,16 @@ package com.example.apptest0523;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,7 +22,11 @@ public class ScanCamera extends AppCompatActivity {
     public Context context;
     SurfaceView surfaceView;
     ScanCamera_SurfaceView scanCameraSurfaceView;
-    ImageView scanaim;
+    ImageView scanaim,changeimage;
+
+    boolean changeimageNum=true;
+
+    Handler handler;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState ) {
@@ -32,15 +40,64 @@ public class ScanCamera extends AppCompatActivity {
         View scancamera_view = inflater.inflate(R.layout.activity_scancamera,null);
         addContentView(scancamera_view,new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
 
-
         scanaim = findViewById(R.id.scanaim);
-                
+        changeimage = findViewById(R.id.changeimage);
+
+        changeimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(changeimageNum){
+                    PrintLog.PrintDebug("직사각형 변환 전 ");
+                    checkImage();
+                    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(550,150);
+                    params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                    scanaim.setLayoutParams(params);
+                    PrintLog.PrintDebug("직사각형 변환 후");
+                    checkImage();
+                    handler.sendEmptyMessageDelayed(10,1000);
+                    changeimageNum = false;
+                }else{
+                    PrintLog.PrintDebug("정사각형 변환 전");
+                    checkImage();
+                    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(350,350);
+                    params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                    scanaim.setLayoutParams(params);
+                    PrintLog.PrintDebug("정사각형 변환 후");
+                    checkImage();
+                    handler.sendEmptyMessageDelayed(10,1000);
+                    changeimageNum =true;
+                }
+            }//onClick
+        });//setOnClickListener
+
+
+        handler = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+
+                switch (msg.what){
+                    case 10:
+                        autocheck();
+                        checkImage();
+                        break;
+                }
+            }
+        };
+
+
+        autocheck();
+
 
     }//onCreate
 
     void checkImage(){
+        scanaim.measure(0,0);
         int aimWidth = scanaim.getMeasuredWidth();
         int aimHeight = scanaim.getMeasuredHeight();
+
+        PrintLog.PrintVerbose("getWidth : " + scanaim.getWidth());
+        PrintLog.PrintVerbose("getHeight : " + scanaim.getHeight());
 
         PrintLog.PrintVerbose("getMeasuredWidth : " + scanaim.getMeasuredWidth());
         PrintLog.PrintVerbose("getMeasuredHeight : " + scanaim.getMeasuredHeight());
@@ -59,6 +116,11 @@ public class ScanCamera extends AppCompatActivity {
 
         PrintLog.PrintVerbose("aim_X : " + aim_X);
         PrintLog.PrintVerbose("aim_Y : " + aim_Y);
+
+    }
+
+    void autocheck(){
+
     }
 
 }//ScanCamera
